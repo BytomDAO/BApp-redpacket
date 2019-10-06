7840259f56970623e53160e4c779365c53cc550e
Zhiting Lin <zlin035@uottawa.ca>
2019-10-06 09:22:20 +0800

    dashboard: forbid the screen scale.

--- redpacket-frontend/src/index.js
+++ redpacket-frontend/src/index.js
@@ -20,6 +20,28 @@ import './i18n';
 require("babel-core/register");
 require("babel-polyfill");
 
+window.onload = function() {
+  let lastTouchEnd = 0;
+  document.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  });
+  document.addEventListener('touchend', function(event) {
+    let now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
+  document.addEventListener('gesturestart', function(event) {
+    event.preventDefault();
+  });
+  document.addEventListener('dblclick', function (event) {
+    event.preventDefault();
+  })
+}
+
 ReactDOM.render((
   <Provider store={configureStore()}>
     <BrowserRouter basename={basename}>
