6ccd00a3218a94360b4a09d6b12200f2edf128f4
Chengcheng Zhang <943420582@qq.com>
2020-06-24 11:15:45 +0800

    update

--- redpacket-backend/service/transaction.go
+++ redpacket-backend/service/transaction.go
@@ -3,8 +3,6 @@ package service
 import (
 	"fmt"
 
-	"github.com/bytom/blockcenter/api/common"
-	"github.com/bytom/blockcenter/api/service"
 	"github.com/bytom/bytom/errors"
 
 	"github.com/redpacket/redpacket-backend/util/types"
@@ -54,20 +52,20 @@ func (s *Service) GetTransaction(req *GetTransactionReq) (*types.Tx, error) {
 	return resp, errors.Wrap(s.request(urlPath, nil, resp), "get transaction")
 }
 
-// ListPendingTxProposals get the bytom pending tx proposals by the blockcenter RPC call
-func (b *bycoinClient) ListTxProposals(guid string) ([]*service.RespTxProposalV3, error) {
-	url := b.baseURL + "/merchant/list-txproposals"
-	req := &service.ListTxProposalsReq{
-		GUIDReq: common.GUIDReq{
-			GUID: guid,
-		},
-		Display: common.Display{
-			Filter: map[string]interface{}{
-				"status": common.TxProposalPendingStatus,
-			},
-		},
-	}
-
-	resp := []*service.RespTxProposalV3{}
-	return resp, errors.Wrap(b.request(url, req, &resp), "ListTxProposals v3")
-}
+//// ListPendingTxProposals get the bytom pending tx proposals by the blockcenter RPC call
+//func (b *bycoinClient) ListTxProposals(guid string) ([]*service.RespTxProposalV3, error) {
+//	url := b.baseURL + "/merchant/list-txproposals"
+//	req := &service.ListTxProposalsReq{
+//		GUIDReq: common.GUIDReq{
+//			GUID: guid,
+//		},
+//		Display: common.Display{
+//			Filter: map[string]interface{}{
+//				"status": common.TxProposalPendingStatus,
+//			},
+//		},
+//	}
+//
+//	resp := []*service.RespTxProposalV3{}
+//	return resp, errors.Wrap(b.request(url, req, &resp), "ListTxProposals v3")
+//}
